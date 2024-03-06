2da45c767be81db0b6326bf99f9fa53faba999c7
jst-r <jstr271@gmail.com>
2024-03-06 14:01:20 +0100

    update styles

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,11 +4,17 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Hello world!</h1>
+      <h1 class="max-6-xs text-6xl text-stone-700 font-thin uppercase my-16">
+        Hello world!
+      </h1>
       <Counter />
       <p class="mt-8">
         Visit{" "}
-        <a href="https://solidjs.com" target="_blank" class="text-sky-600 hover:underline">
+        <a
+          href="https://solidjs.com"
+          target="_blank"
+          class="text-stone-600 hover:underline"
+        >
           solidjs.com
         </a>{" "}
         to learn how to build Solid apps.
@@ -16,7 +22,7 @@ export default function Home() {
       <p class="my-4">
         <span>Home</span>
         {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
+        <A href="/about" class="text-stone-600 hover:underline">
           About Page
         </A>{" "}
       </p>
